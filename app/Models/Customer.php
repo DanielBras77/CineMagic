f691754ffe2f6c7e86dbfd5a0e5a0ba7f386e5c1
Daniel Brás <danielbras18@gmail.com>
2024-05-10 16:00:39 +0100

    Customer

--- app/Models/Customer.php
+++ app/Models/Customer.php
@@ -4,8 +4,17 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Customer extends Model
 {
-    use HasFactory;
+    use HasFactory,SoftDeletes;
+    public $incrementing=false;
+    protected $fillable=['id', 'nif','payment_type', 'payment_ref'];
+
+    public function user():BelongsTo
+    {
+        return $this->belongsTo(User::class, 'id', 'id')->withTrashed();
+    }
 }
