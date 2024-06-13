aeed3a3dd32b9c09cd0126562809af7ed15d844d
degor <diogogomes600@gmail.com>
2024-06-13 13:00:31 +0100

    Fui buscar as coisas do rodrigo

--- app/Http/Controllers/PurchaseController.php
+++ app/Http/Controllers/PurchaseController.php
@@ -49,3 +49,61 @@ class PurchaseController extends Controller
         //
     }
 }
+/*<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\PriceRequest;
+use App\Models\Price;
+use Illuminate\Http\Request;
+
+class PriceController extends Controller
+{
+
+    public function edit()
+    {
+        //
+    }
+
+
+    public function update(PriceRequest $request)
+    {
+        $prices = Price::first();
+        $unitPriceCatalog = $request->input('unit_price_catalog',$prices->unit_price_catalog);
+        $unitPriceOwn = $request->input('unit_price_own',$prices->unit_price_own);
+        $unitPriceCatalogDiscount = $request->input('unit_price_catalog_discount',$prices->unit_price_catalog_discount);
+        $unitPriceOwnDiscount = $request->input('unit_price_own_discount',$prices->unit_price_own_discount);
+        $qtyDiscount = $request->input('qty_discount',$prices->qty_discount);
+
+        // Put the value in the DB only if the value was passes in the request
+        $unitPriceCatalog == null ?: $prices->unit_price_catalog= $unitPriceCatalog;
+        $unitPriceOwn == null ?: $prices->unit_price_own= $unitPriceOwn;
+        $unitPriceCatalogDiscount == null ?: $prices->unit_price_catalog_discount= $unitPriceCatalogDiscount;
+        $unitPriceOwnDiscount == null ?: $prices->unit_price_own_discount= $unitPriceOwnDiscount;
+        $qtyDiscount == null ?: $prices->qty_discount = $qtyDiscount;
+
+        $prices->save();
+        $htmlMessage = "Prices was successfully updated!";
+        return redirect()
+            ->route('dashboard.index')
+            ->with('alert-msg', $htmlMessage)
+            ->with('alert-type', 'success');
+
+    }
+    public static function unitPrice($qtd, $propria){
+        $price = Price::first();
+        if($propria){
+            if($qtd >= $price->qty_discount){
+                return $price->unit_price_own_discount;
+            }else{
+                return $price->unit_price_own;
+            }
+
+        }else{
+            if($qtd >= $price->qty_discount)
+                return $price->unit_price_catalog_discount;
+        }
+        return $price->unit_price_catalog;
+    }
+}
+*/
--- app/Http/Controllers/StatisticsController.php
+++ app/Http/Controllers/StatisticsController.php
@@ -71,3 +71,105 @@ class StatisticsController extends \Illuminate\Routing\Controller
         return $query->first();
     }
 }
+/*<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Order;
+use App\Models\Category;
+use App\Models\Price;
+use App\Models\Order_item;
+use App\Models\Tshirt_image;
+use App\Models\User;
+use App\Charts\Orders;
+use App\Charts\Categories;
+use Carbon\Carbon;
+use Illuminate\Support\Collection;
+
+class DashboardController extends Controller
+{
+    public function index()
+    {
+        $monthlyTotalPrices = Order::selectRaw('SUM(total_price) as total_price_sum, DATE_FORMAT(date, "%Y-%m") as month')
+            ->where('status', '!=', 'canceled')
+            ->groupBy('month')
+            ->orderBy('month', 'asc')
+            ->pluck('total_price_sum');
+
+        $currentMonth = Carbon::now();
+        $startMonth = Carbon::create(2020, 8, 1);
+
+        $monthsCollection = Collection::make();
+
+        while ($startMonth->lessThanOrEqualTo($currentMonth)) {
+            $monthsCollection->push($startMonth->format('Y-m'));
+            $startMonth->addMonth();
+        }
+
+        $Orderschart = new Orders;
+        $Orderschart->labels($monthsCollection)
+            ->dataset('Total Earnings','bar' ,$monthlyTotalPrices)
+            ->backgroundColor('rgb(54, 162, 235)');
+
+
+        $totalSales = Order_item::join('tshirt_images', 'order_items.tshirt_image_id', '=', 'tshirt_images.id')
+            ->leftJoin('categories', 'tshirt_images.category_id', '=', 'categories.id')
+            ->selectRaw('IFNULL(categories.name, "Custom Tshirts") AS category, SUM(order_items.qty) AS total_sales')
+            ->join('orders', 'order_items.order_id', '=', 'orders.id')
+            ->where('orders.status', 'closed')
+            ->groupBy('category')
+            ->get();
+        $categories = $totalSales->pluck('category');
+        $sales = $totalSales->pluck('total_sales');
+
+        foreach ($sales as $sale) {
+            $red = mt_rand(0, 255);
+            $green = mt_rand(0, 255);
+            $blue = mt_rand(0, 255);
+
+            $colors[] = "rgb($red, $green, $blue)";
+        }
+
+        $categoriesChart= new Categories;
+        $categoriesChart->labels($categories)
+            ->dataset('Sales','pie',$sales)
+            ->backgroundColor($colors)
+            ->options([
+                'scales' => [
+                    'y' => [
+                        'ticks' => [
+                            'display' => false
+                        ]
+                    ]
+                ]
+            ]);
+
+        $prices=Price::first();
+
+        $numAdmins = User::where('user_type', 'A')
+            ->where('blocked', 0)
+            ->whereNull('deleted_at')
+            ->count();
+
+        $numEmployees = User::where('user_type', 'E')
+            ->where('blocked', 0)
+            ->whereNull('deleted_at')
+            ->count();
+
+        $numCustomers = User::where('user_type', 'C')
+            ->where('blocked', 0)
+            ->whereNull('deleted_at')
+            ->count();
+
+        $numBlocked = User::where('blocked', 1)
+            ->whereNull('deleted_at')
+            ->count();
+
+        $numCategories = Category::whereNull('deleted_at')->count();
+
+        $numSales = $sales->sum();
+
+        return view('dashboard.index', compact('Orderschart','categoriesChart','prices','numAdmins','numEmployees','numCustomers','numBlocked','numCategories', 'numSales'));
+    }
+}
+*/
